471459266705d5d6e91bd5464ed37fbb8d4c94e3
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-05 10:50:25 +0000

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
+""" The basics of async """
 
-from typing import List
 import asyncio
-import random
+from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    
-    spawn_list = []
-    delay_list = []
-    for i in range(n):
-        delayed_task = task_wait_random(max_delay)
-        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
-        spawn_list.append(delayed_task)
-
-    for spawn in spawn_list:
-        await spawn
-
-    return delay_list
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawn task_wait_random n times with the specified max_delay.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
